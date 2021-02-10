ee8dd324e6e4c592291969688572faef1369a853
Lukas Lobnig <lukas.lobnig@gmail.com>
2021-02-10 11:41:48 +0100

    Requirements.txt

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
+
+# sg.Window(title="Price Sccraper", layout=[[]], margins=(100,50)).read()
+
 URL = 'https://www.amazon.de/Nike-Chelsea-Pre-Match-Jersey-Concord/dp/B08KJGMCN2/ref=sr_1_1_sspa?dchild=1&keywords=chelsea+trikot&qid=1612952062&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyNjlGUTdPVlMzVUhLJmVuY3J5cHRlZElkPUEwOTM2MDM1MkJWQURGSVQ0UUhRMSZlbmNyeXB0ZWRBZElkPUEwOTg0MDM0M0oyV1BaQ0hUU0MxVSZ3aWRnZXROYW1lPXNwX2F0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU='
 
 URL=input("Paste your Amazon Link: \n")
